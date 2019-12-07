77810cc73162ff6c3d178e34a383589e67982073
Nedzad Alibegovic <git@nedzad.dev>
2019-12-08 00:39:11 +0100

    Add console output for better readabilty

--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@ using namespace std;
 
 int main() {
 #pragma region ListSeq
+    cout << "ListSeq\n";
     ListSeq<int> list_seq;
 
     for (size_t i = 0; i < 300; i++) {
@@ -28,12 +29,15 @@ int main() {
 
     list_seq.popFront();
 
-    std::cout << list_seq.popBack() << "\n";
+    cout << list_seq.popBack() << "\n";
 
     list_seq.print();
+
+    cout << "/ListSeq\n";
 #pragma endregion
 
 #pragma region ListPtr
+    cout << "ListPtr\n";
     ListPtr<int> list_ptr;
 
     for (size_t i = 0; i < 100; i++) {
@@ -49,9 +53,12 @@ int main() {
     list_ptr.pushBack(1337);
 
     list_ptr.print();
+
+    cout << "/ListPtr\n";
 #pragma endregion
 
 #pragma region StackSeq
+    cout << "StackSeq\n";
     StackSeq<int> stack_seq;
 
     for (size_t i = 0; i < 100; i++) {
@@ -61,9 +68,12 @@ int main() {
     stack_seq.pop();
 
     stack_seq.print();
+
+    cout << "/StackSeq\n";
 #pragma endregion
 
 #pragma region StackPtr
+    cout << "StackPtr\n";
     StackPtr<int> stack_ptr;
 
     for (size_t i = 0; i < 100; i++) {
@@ -73,11 +83,13 @@ int main() {
     stack_ptr.pop();
 
     stack_ptr.print();
+
+    cout << "/StackPtr\n";
 #pragma endregion
 
 #pragma region QueueSeq
+    cout << "QueueSeq\n";
     QueueSeq<int> queue_seq;
-
 
     for (size_t i = 1; i < 10; i++) {
         queue_seq.enqueue(i);
@@ -86,9 +98,12 @@ int main() {
     queue_seq.dequeue();
 
     queue_seq.print();
+
+    cout << "/QueueSeq\n";
 #pragma endregion
 
 #pragma region QueuePtr
+    cout << "QueuePtr\n";
     QueuePtr<int> queue_ptr;
 
     for (size_t i = 1; i < 10; i++) {
@@ -100,9 +115,12 @@ int main() {
     for (size_t i = 0; i < 9; i++) {
         cout << queue_ptr.dequeue() << "<-\n";
     }
+
+    cout << "/QueuePtr\n";
 #pragma endregion
 
 #pragma region PriorityQueueSeq
+    cout << "PriorityQueueSeq\n";
     PriorityQueueSeq<int> priority_queue_seq;
 
     for (size_t i = 0; i < 10; i++) {
@@ -115,9 +133,11 @@ int main() {
         cout << priority_queue_seq.dequeue() << "<-\n";
     }
 
+    cout << "/PriorityQueueSeq\n";
 #pragma endregion
 
 #pragma region PriorityQueuePtr
+    cout << "PriorityQueuePtr\n";
     PriorityQueuePtr<int> priority_queue_ptr;
 
     for (size_t i = 10; i > 0; i--) {
@@ -129,6 +149,8 @@ int main() {
     for (size_t i = 0; i < 10; i++) {
         cout << priority_queue_ptr.dequeue() << "<-\n";
     }
+
+    cout << "/PriorityQueuePtr\n";
 #pragma endregion
 
 #pragma region BubbleSort
@@ -136,22 +158,23 @@ int main() {
 
     BubbleSort(arr_bubble.data(), arr_bubble.size());
 
-    cout << "\nBubble:\n";
+    cout << "\nBubble sort:\n";
     for (size_t i = 0; i < arr_bubble.size(); i++) {
         cout << arr_bubble[i] << " ";
     }
-#pragma endregion
 
+#pragma endregion
 
 #pragma region InsertionSort
     vector<int> arr_insertion{ 76, 22, 71, 52, 93, 38, 79, 14, 6, 21, 42, 23, 18, 70, 81, 51, 20, 63, 89, 68, 9, 95, 31, 11, 12, 13, 37, 94, 27, 50, 64, 62, 34, 54, 25, 92, 2, 77, 7, 90, 69, 46, 58, 59, 60, 16, 74, 73, 53, 97, 26, 48, 45, 5, 33, 15, 17, 29, 61, 57, 82, 44, 30, 4, 98, 83, 86, 80, 49, 65, 35, 43, 78, 85, 19, 87, 96, 39, 40, 36, 91, 1, 28, 100, 75, 84, 55, 32, 24, 41, 99, 8, 3, 88, 10, 47, 56, 66, 72, 67 };
 
     InsertionSort(arr_insertion.data(), arr_insertion.size());
 
-    cout << "\nInsertion:\n";
+    cout << "\nInsertion sort:\n";
     for (size_t i = 0; i < arr_insertion.size(); i++) {
         cout << arr_insertion[i] << " ";
     }
+
 #pragma endregion
 
 #pragma region SelectionSort
@@ -159,10 +182,11 @@ int main() {
 
     SelectionSort(arr_selection.data(), arr_selection.size());
 
-    cout << "\nSelection:\n";
+    cout << "\nSelection sort:\n";
     for (size_t i = 0; i < arr_selection.size(); i++) {
         cout << arr_selection[i] << " ";
     }
+
 #pragma endregion
 
     return 0;
